021ca2c45781c6ae09729f9a363af3aca640c134
Zac Solomon <zac@zacsolo.com>
2020-09-08 13:26:14 -0400

    express and firebase setup

---
+++ social-media-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'This is the scream body',
+      createdAt: '2020-09-08T17:18:14.584Z',
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
