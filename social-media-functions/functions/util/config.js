4fb53c66ba665512322690b6ca0dc1c8584a3f52
Zac Solomon <zac@zacsolo.com>
2020-09-09 15:41:49 -0400

    refactored into modules

---
+++ social-media-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyBX8kgkHESbd7uV9jvSOHcT-MORbCosj5U',
+  authDomain: 'social-media-with-fire.firebaseapp.com',
+  databaseURL: 'https://social-media-with-fire.firebaseio.com',
+  projectId: 'social-media-with-fire',
+  storageBucket: 'social-media-with-fire.appspot.com',
+  messagingSenderId: '705779469883',
+  appId: '1:705779469883:web:6472837f68b4d57975561e',
+  measurementId: 'G-MVLP5GX714',
+};
