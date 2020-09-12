a05b3bf2170f8585ec3fa14b394caddae6f477b6
Zac Solomon <zac@zacsolo.com>
2020-09-12 14:48:04 -0400

    navbar and initial React Router routes

--- social-client/src/App.js
+++ social-client/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/home';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Our app</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <div className='container'>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/login' component={Login} />
+          <Route path='/signup' component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
