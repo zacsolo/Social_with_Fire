db883ebe5c8cc0acb863be9617594fd3d307fb32
Zac Solomon <zac@zacsolo.com>
2020-09-15 14:26:41 -0400

    Login form

--- social-client/src/pages/login.js
+++ social-client/src/pages/login.js
@@ -1,13 +1,152 @@
 import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+import AppIcon from '../images/icon.png';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+
+// MUI
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { TextField } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const styles = {
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    maxWidth: 100,
+    margin: '20px auto 20px auto',
+  },
+  pageTitle: {
+    margin: '10px auto 10px auto',
+  },
+  textField: {
+    margin: '10px auto 10px auto',
+  },
+  button: {
+    marginTop: 20,
+    height: 35,
+    width: 75,
+    position: 'relative',
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10,
+  },
+  progress: {
+    position: 'absolute',
+  },
+};
 
 export class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    Axios.post('/login', userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({ loading: false });
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (e) => {
+    this.setState({ ...this.state, [e.target.name]: e.target.value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} alt='monkey' className={classes.image} />
+          <Typography variant='h2' className={classes.pageTitle}>
+            Title
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id='email'
+              name='email'
+              type='email'
+              label='Email'
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id='password'
+              name='password'
+              type='password'
+              label='Password'
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.general && (
+              <Typography variant='body2' className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button
+              type='submit'
+              variant='contained'
+              color='primary'
+              className={classes.button}
+              disabled={loading}>
+              {loading ? (
+                <CircularProgress
+                  className={classes.progress}
+                  size={20}></CircularProgress>
+              ) : (
+                'Login'
+              )}
+            </Button>
+            <br />
+            <small>
+              don't have an account? sign up <Link to='/signup'> here</Link>
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
